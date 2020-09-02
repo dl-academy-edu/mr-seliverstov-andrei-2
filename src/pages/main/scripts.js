e895679e4d901626d103a1f5ea08824b6f052e2e
Uber1337mensch <seliverstovandrey88@mail.ru>
2020-09-03 02:38:00 +0400

    Адаптивный хедер и меню навигации для планшетов

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,7 +1,8 @@
 var popup = document.querySelector(".modal_js"),
     buttonOpen = document.querySelector(".button-open_js"),
     exitButton = document.querySelector(".exit-button_js"),
-    inputText = document.querySelector(".input-text_js");
+    inputText = document.querySelector(".input-text_js"),
+    html = document.querySelector("html");
 
 buttonOpen.addEventListener("click", function() {
     popup.classList.add("modal-background_open");
@@ -19,4 +20,16 @@ window.addEventListener("keydown", function(e) {
     if(e.code === "Escape" && popup.classList.contains("modal-background_open")){
         popup.classList.remove("modal-background_open")
     }
-})
+});
+
+
+var buttonOpenMenu = document.getElementById(".js-button-open-menu"),
+    buttonCloseMenu = document.getElementById(".js-button-close-menu");
+
+buttonOpenMenu.addEventListener("click", function() {
+    html.classList.add("mobile-menu__open");
+});
+
+buttonCloseMenu.addEventListener("click", function() {
+    html.classList.remove("mobile-menu__open");
+});
